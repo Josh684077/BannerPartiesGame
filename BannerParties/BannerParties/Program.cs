c538364dc7de549f9eff457420adf28d809dc913
Joshua Andrea <90309312+Josh684077@users.noreply.github.com>
2022-03-11 19:26:32 +0100

    Add files via upload

---
+++ BannerParties/BannerParties/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BannerParties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program maingame = new Program();
+            maingame.Start();
+        }
+
+        void Start()
+        {
+             
+        }
+    }
+}
